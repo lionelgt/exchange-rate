f803b512d5ab0d20ed487bce829d1fbfb089b486
lgonzales <lionel.gonzalest@gmail.com>
2020-10-22 23:26:47 -0500

    First commit: calculate and update exchange rate,

---
+++ bcp-exchange-rate-boot/src/main/java/pe/com/bcp/echangerate/boot/ExchangeRateApplication.java
@@ -0,0 +1,14 @@
+package pe.com.bcp.echangerate.boot;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import pe.com.bcp.exchangerate.config.EnableExchangeRate;
+
+@EnableExchangeRate
+@SpringBootApplication
+public class ExchangeRateApplication {
+	public static void main(String[] args) {
+		SpringApplication.run(ExchangeRateApplication.class, args);
+	}
+
+}
